b3fc0b86f1aaba3a21e1cae00330c5feac535736
Denis Coelho <deniscoelho.dev@gmail.com>
2022-02-17 13:22:09 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BsStar, BsStarFill} from 'react-icons/bs'
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box one">
+        <div className="details">
+          <div className="topic">Description</div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum laborum nulla delectus illum dolor nemo quo impedit minima. Architecto rerum consectetur voluptas molestiae assumenda temporibus iure tempore fugit illum corrupti?</p>
+          <div className="rating">
+            <BsStarFill/>
+            <BsStarFill/>
+            <BsStarFill/>
+            <BsStarFill/>
+            <BsStarFill/>
+            <BsStar/>
+            <BsStar/>
+          </div>
+          <div className="price-box">
+            <div className="discount">R$ 1800.00</div>
+            <div className="price">R$ 2500.00</div>
+          </div>
+        </div>
+        <div className="button1">
+          <button>Adicionar</button>
+        </div>
+      </div>
+      <div className="box two">
+        <div className="image-box">
+          <div className="image">
+            <img src="images/camera.png" alt="" />
+          </div>
+          <div className="info">
+            <div className="brand">SONY</div>
+            <div className="name">SONY ALPHA A7 KIT</div>
+            <div className="shipping">Frete Grátis</div>
+            <div className="button2">
+              <button>Mais</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
